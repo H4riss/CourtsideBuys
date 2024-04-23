7bdd900d7128e89c20eae61b4ed6378b501257c7
Haris <harismukangad@gmail.com>
2024-04-23 11:26:00 -0400

    Initial

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,13 @@
+export const environment = {
+    production: false,
+    backendUrl: "http://localhost:4200/",
+    firebaseConfig: {
+        apiKey: "AIzaSyCakA_KEPAWtqOMhGsWn3ueENmLVYHJcCA",
+        authDomain: "courtside-buys.firebaseapp.com",
+        projectId: "courtside-buys",
+        storageBucket: "courtside-buys.appspot.com",
+        messagingSenderId: "562078878348",
+        appId: "1:562078878348:web:7ed93e32c625238926c9a9",
+        measurementId: "G-JD3S3TSJF7"
+      }
+};
