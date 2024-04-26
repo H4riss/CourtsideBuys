4f8c653f9015e3c6eab759a2f0b16f02dc8552aa
Haris <harismukangad@gmail.com>
2024-04-26 10:28:33 -0400

    popupchanged

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,35 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
 import { NgModule } from "@angular/core";
+import { PopupService } from './popup/popup.service';
+import { MatDialogModule } from '@angular/material/dialog';
+// import { Router } frp0: stringp0: stringp0: stringom 'express';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,RouterLink],
+  imports: [RouterOutlet,RouterLink,MatDialogModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'CourtsideKicksBuys';
+  constructor(private popupService: PopupService) {}
+  // constructor(private router:PopupService){}
+  // // constructor(private router: Router){}
+  closeDialog() {
+  }
 numberOfItems: any;
+
+
+
+openPopup() {
+  // this.router.openPopup(['/popup']);
+  this.popupService.openPopup('popup');
+  // this.router.navigateByUrl('/categories')
+}
+
 
 }
 
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,8 @@
 import { Routes } from '@angular/router';
-import { PhotoguideComponent } from './photoguide/photoguide.component';
+// import { PhotoguideComponent } from './photoguide/photoguide.component';
 import { AppComponent } from './app.component';
 
 export const routes: Routes = [
-    {path:'photoguide', component: PhotoguideComponent},
+    // {path:'photoguide', component: PhotoguideComponent},
     
 ];
