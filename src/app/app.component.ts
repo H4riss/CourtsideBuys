bd2000a54bfa39940c1dc0d160560806ecc3544e
Haris <harismukangad@gmail.com>
2024-05-01 11:54:45 -0400

    mall

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
 import { NgModule } from "@angular/core";
 import { PopupService } from './popup/popup.service';
 import { MatDialogModule } from '@angular/material/dialog';
+import { Firestore, collection, getDocs } from '@angular/fire/firestore';
+import { FormsModule } from '@angular/forms';
+import { Observable } from 'rxjs';
+import { AngularFirestore, AngularFirestoreDocument} from '@angular/fire/compat/firestore';
 // import { Rop0: stringuter } frp0: stringp0: stringp0: stringom 'express';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,RouterLink,MatDialogModule],
+  imports: [RouterOutlet,RouterLink,MatDialogModule,FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'CourtsideKicksBuys';
+export class AppComponent implements OnInit {
   constructor(private popupService: PopupService) {}
-  // constructor(private router:PopupService){}
-  // // constructor(private router: Router){}
+  title = 'CourtsideKicksBuys';
+  firestore = inject(Firestore);
+  public email: String = "";
+  public name: String = "";
+  public price: String = "";
+  public size: String = "";
+  public id: String = "";
+ 
+  tutorial: AngularFirestoreDocument<any> | undefined
+
+  ngOnInit(){
+    // Observable<any> tutorial = this.firestore.doc("users");
+    // getDocs(collection(this.firestore,"users")).then((response) => {
+    //   console.log("reached firebase success",response.docs); });
+    }
   
+
+
   closeDialog() {
   }
 numberOfItems: any;
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,4 +1,4 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 // import { routes } from './routes';
@@ -6,7 +6,15 @@ import { routes } from './app.routes';
 import { RouterModule, Routes } from '@angular/router';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getStorage, provideStorage } from '@angular/fire/storage';
+import { environment } from '../environments/environment.development';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration(), provideAnimationsAsync()]
+  providers: [provideRouter(routes), provideClientHydration(), provideAnimationsAsync(),
+     importProvidersFrom(provideFirebaseApp(() => initializeApp(environment.firebaseConfig))), importProvidersFrom(provideAuth(() => getAuth())), importProvidersFrom(provideFirestore(() => getFirestore())), importProvidersFrom(provideStorage(() => getStorage()))]
 };
+
+
